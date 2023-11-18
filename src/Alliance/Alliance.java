ac60c90b299666c5522db3a418b8e7dc61af61b0
Syed Ali Rizwan <alirizwan2003@gmail.com>
2023-11-18 12:38:01 +0500

    10

--- src/Alliance/Alliance.java
+++ src/Alliance/Alliance.java
@@ -1,6 +1,18 @@
 package Alliance;
 
 public enum Alliance {
-    BLACK,
-    WHITE
+    BLACK {
+        @Override
+        public int getDirection() {
+            return 1;
+        }
+    },
+    WHITE {
+        @Override
+        public int getDirection() {
+            return -1;
+        }
+    };
+
+    public abstract int getDirection();
 }
