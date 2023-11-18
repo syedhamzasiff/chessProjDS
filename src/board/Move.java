8adbfd80a10adc3e39a507dc4007154755f9490e
syedhamzasiff <syedhamzasiff@gmail.com>
2023-11-19 02:03:22 +0500

    21

--- src/board/Move.java
+++ src/board/Move.java
@@ -3,7 +3,6 @@ package board;
 import pieces.Piece;
 
 public abstract class Move {
-
     final Board board;
     final Piece movedPiece;
     final int destinationCoordinate;
@@ -13,24 +12,58 @@ public abstract class Move {
         this.movedPiece = movedPiece;
         this.destinationCoordinate = destinationCoordinate;
     }
+    public int getDestinationCoordinate(){
+        return this.destinationCoordinate;
+    }
+    public abstract Board execute();
 
-    public static final class MajorMove extends Move {
+public static final class MajorMove extends Move {
 
         public MajorMove(final Board board, final Piece movedPiece, final int destinationCoordinate) {
             super(board, movedPiece, destinationCoordinate);
         }
 
+        /*
+        performing a move doesnt mean you are going to mutate the board, it means that you will materialise a new board to existence
+        move has a handle coming into this new member field
+        when you execute a move, it is going to return a new board, not mutate the final board that was passed
+
+         */
+        @Override
+        public Board execute() {
+            //return a new Board from execute
+            final Builder builder = new Builder();
+            for (final Piece piece: this.board.currentPlayer().getActivePieces()){//traverse through all the current pieces
+                //TODO hashcode and equals for pieces
+                if (!this.movedPiece.equals(piece)){ //the not moved pieces must be just placed on the new outbound board
+                    builder.setPiece(piece);
+                }
+            }
+            for (final Piece piece: this.board.currentPlayer().getOpponent().getActivePieces()) {
+                //no if statement because they won't have a moved piece
+                builder.setPiece(piece); //same thing for the enemy pieces
+            }
+            //move the moved piece
+            builder.setPiece(null);
+            //set the move maker to the opponent for the next turn
+            builder.setMoveMaker(this.board.currentPlayer().getOpponent().getAlliance());
+            return builder.build();
+        }
     }
 
-    public static final class AttackMove extends Move {
-
+public static final class AttackMove extends Move {
         final Piece attackedPiece;
-
         public AttackMove(final Board board, final Piece movedPiece, final int destinationCoordinate, final Piece attackedPiece) {
             super(board, movedPiece, destinationCoordinate);
             this.attackedPiece = attackedPiece;
         }
+
+        @Override
+        public Board execute() {
+            return null;
+        }
     }
+
 
 
 }
