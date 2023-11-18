1f8fb4456094c3c86419b5b845c8930ae49cf575
Syed Ali Rizwan <alirizwan2003@gmail.com>
2023-11-18 11:43:49 +0500

    7

---
+++ src/pieces/Bishop.java
@@ -0,0 +1,62 @@
+package pieces;
+
+import Alliance.Alliance;
+import board.Board;
+import board.BoardUtils;
+import board.Move;
+import board.Tile;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Bishop extends Piece{
+
+    private static final int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9, -7, 7, 9}; // possible positions for the move
+
+    Bishop(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int candidateCoordinateOffset : CANDIDATE_MOVE_VECTOR_COORDINATES) {
+            int candidateDestinationCoordinate = this.piecePosition;
+
+            while (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) { // loop each of the vectors in the constant array and check if its valid position (not off the board) and if the position is valid then check if the position is occupied. if not occupied its a majormove else its a attackmove
+
+                if (isFirstColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset) || isEighthColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset)) {
+                    break;
+                }
+
+                candidateDestinationCoordinate += candidateCoordinateOffset;
+                if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                    if (!candidateDestinationTile.isTileOccupied()) {
+                        legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                    } else {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+                        if (this.pieceAlliance != pieceAlliance) { // means that the piece is enemy's piece
+                            legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                        break; // the break ensures that if a blocking piece is in the way
+                    }
+                }
+            }
+        }
+        //return ImmutableList.copyOf(legalMoves);
+        return null; // had to do this cos line 46 giving error
+    }
+
+    private static boolean isFirstColumnExclusion (final int currentPosition, final int candidateOffset) {
+        return BoardUtils.FIRST_COLUMN[currentPosition] && ((candidateOffset == -9) || (candidateOffset == 7));
+    }
+
+    private static boolean isEighthColumnExclusion (final int currentPosition, final int candidateOffset) {
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && ((candidateOffset == -7) || (candidateOffset == 9));
+    }
+}
