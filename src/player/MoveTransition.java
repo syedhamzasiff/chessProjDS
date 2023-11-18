eabd59d179b8c0adde7986927438e38f58ec3ffc
Syed Ali Rizwan <alirizwan2003@gmail.com>
2023-11-19 02:06:58 +0500

    19

--- src/player/MoveTransition.java
+++ src/player/MoveTransition.java
@@ -3,6 +3,8 @@ package player;
 import board.Board;
 import board.Move;
 
+import java.util.concurrent.Future;
+
 public class MoveTransition {
 
     private final Board transitionBoard;
@@ -13,5 +15,9 @@ public class MoveTransition {
         this.transitionBoard = transitionBoard;
         this.move = move;
         this.moveStatus = moveStatus;
+    }
+
+    public MoveStatus getMoveStatus() {
+        return this.moveStatus;
     }
 }
--- src/player/Player.java
+++ src/player/Player.java
@@ -6,18 +6,32 @@ import board.Move;
 import pieces.King;
 import pieces.Piece;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public abstract class Player {
 
     protected final Board board;
     protected final King playerKing;
     protected final Collection<Move> legalMoves;
+    private final boolean isInCheck;
 
     Player(final Board board, final Collection<Move> legalMoves, final Collection<Move> opponentMoves) {
         this.board = board;
         this.playerKing = establishKing();
         this.legalMoves = legalMoves;
+        this.isInCheck = !Player.calculateAttacksOnTile(this.playerKing.getPiecePosition(), opponentMoves).isEmpty();
+    }
+
+    private static Collection<Move> calculateAttacksOnTile(int piecePosition, Collection<Move> moves) {
+        final List<Move> attackMoves = new ArrayList<>();
+        for (final Move move : moves) {
+            if (piecePosition == move.getDestinationCoordinate()) {
+                attackMoves.add(move);
+            }
+        }
+        return List.copyOf(attackMoves);
     }
 
     private King establishKing() {
@@ -34,11 +48,25 @@ public abstract class Player {
     }
 
     public boolean isInCheck() {
-        return false;
+        return this.isInCheck;
     }
 
     public boolean isInCheckMate() {
+        return this.isInCheck && !hasEscapeMoves();
+    }
+
+    private boolean hasEscapeMoves() {
+        for (final Move move : this.legalMoves) {
+            final MoveTransition transition = makeMove(move);
+            if (transition.getMoveStatus().isDone()) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public boolean isInStaleMate() {
+        return !this.isInCheck && !hasEscapeMoves();
     }
 
     public boolean isCastled() {
@@ -47,10 +75,6 @@ public abstract class Player {
 
     public MoveTransition makeMove (final Move move) {
         return null;
-    }
-
-    public boolean isInStaleMate() {
-        return false;
     }
 
     public abstract Collection<Piece> getActivePieces ();
